9dc519b037fed358acfaf4f34ad6bb9463a6d6f6
Nicolas Suzor <nic@suzor.com>
2020-08-01 09:46:18 +1000

    autoshift on, double tap off

--- keyboards/keebio/iris/keymaps/nicsuzor/nic.c
+++ keyboards/keebio/iris/keymaps/nicsuzor/nic.c
@@ -83,6 +83,30 @@ void ctrl_dance(qk_tap_dance_state_t *state, void *user_data);
  * For the third point, there does exist the 'DOUBLE_SINGLE_TAP', however this is not fully tested
  *
  */
+
+ uint8_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return SINGLE_TAP;
+        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+        else return SINGLE_HOLD;
+    } else if (state->count == 2) {
+        // DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return DOUBLE_HOLD;
+        else return DOUBLE_TAP;
+    }
+
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TRIPLE_TAP;
+        else return TRIPLE_HOLD;
+    } else return 8; // Magic number. At some point this method will expand to work for more presses
+}
+
 void ctrl_dance(qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 2 && state->pressed) {
     register_code16(KC_LCTRL);
@@ -95,11 +119,12 @@ void ctrl_dance(qk_tap_dance_state_t *state, void *user_data) {
     tap_code(state->keycode);
   }
 }
+
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [KC_Z] = ACTION_TAP_DANCE_FN(ctrl_dance),
+/*    [KC_Z] = ACTION_TAP_DANCE_FN(ctrl_dance),
     [KC_X] = ACTION_TAP_DANCE_FN(ctrl_dance),
     [KC_C] = ACTION_TAP_DANCE_FN(ctrl_dance),
-    [KC_V] = ACTION_TAP_DANCE_FN(ctrl_dance)
+    [KC_V] = ACTION_TAP_DANCE_FN(ctrl_dance)*/
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
@@ -141,7 +166,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
       }
       break;
-    case _ADJUST:
+    case _RAISE:
         if (clockwise) {
           tap_code(KC_RIGHT);
         } else {
