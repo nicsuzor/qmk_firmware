02ada4a65405a7cc3e9c8d5d8019747b3333e89a
Nicolas Suzor <nic@suzor.com>
2020-08-03 22:27:32 +1000

    nic iris working

--- keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/config.h
+++ keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/config.h
@@ -23,3 +23,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 // #define USE_I2C
 #define EE_HANDS
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_ALPHA 
