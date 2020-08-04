5b613653efa6adf9987692257a6c8cfbf11ffa44
Nicolas Suzor <nic@suzor.com>
2020-08-04 13:45:33 +1000

    no number keys, qwerty top row

--- keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/iris1/config.h
+++ keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/iris1/config.h
@@ -24,4 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 #define NO_AUTO_SHIFT_SPECIAL
-#define NO_AUTO_SHIFT_ALPHA 
+#define NO_AUTO_SHIFT_ALPHA
+
+//https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
--- keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/iris1/rules.mk
+++ keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/iris1/rules.mk
@@ -4,6 +4,10 @@ AUTO_SHIFT_ENABLE = yes
 RGBLIGHT_SPLIT = yes
 RGBLIGHT_ANIMATIONS = no
 RGBLIGHT_LAYERS = no
-TAP_DANCE_ENABLE = no
+TAP_DANCE_ENABLE = yes
+#MOUSEKEY_ENABLE = no
+
+# https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+#EXTRAFLAGS += -flto
 #AUDIO_ENABLE = yes
 SRC += nic.c
