3c644d99c1bc58b3f165883f3b9e8c1d2a60fe52
Nicolas Suzor <nic@suzor.com>
2020-08-05 10:24:24 +1000

    moved arrows to right hand

--- keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/iris1/rules.mk
+++ keyboards/keebio/iris/keymaps/nicsuzor/qmk-nicsuzor/iris1/rules.mk
@@ -4,7 +4,7 @@ AUTO_SHIFT_ENABLE = yes
 RGBLIGHT_SPLIT = yes
 RGBLIGHT_ANIMATIONS = no
 RGBLIGHT_LAYERS = no
-TAP_DANCE_ENABLE = yes
+#TAP_DANCE_ENABLE = yes
 #MOUSEKEY_ENABLE = no
 
 # https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
