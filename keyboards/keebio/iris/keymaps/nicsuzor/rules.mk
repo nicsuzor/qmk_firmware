eb6ab3113140482f0c2bf028d2758f4060555bc6
Nicolas Suzor <nic@suzor.com>
2020-08-27 14:05:38 +1000

    moved ctrl alt

--- keyboards/keebio/iris/keymaps/nicsuzor/rules.mk
+++ keyboards/keebio/iris/keymaps/nicsuzor/rules.mk
@@ -6,6 +6,7 @@ RGBLIGHT_ANIMATIONS = no
 RGBLIGHT_LAYERS = no
 #TAP_DANCE_ENABLE = yes
 #MOUSEKEY_ENABLE = no
+NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 
 # https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
 #EXTRAFLAGS += -flto
