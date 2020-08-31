6f5c221c090245152f9716a73f3f148d130a6035
Nicolas Suzor <nic@suzor.com>
2020-08-31 15:24:48 +1000

    working oled

--- keyboards/lily58/keymaps/nicsuzor/config.h
+++ keyboards/lily58/keymaps/nicsuzor/config.h
@@ -31,10 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT
-
-#define FORCE_NKRO
+#define TAPPING_TERM 100
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
@@ -43,11 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-
-//#define SSD1306OLED
-
-#undef OLED_DRIVER_ENABLE
-#define OLED_DRIVER_ENABLE
 
 // Underglow
 /*
