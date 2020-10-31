f6c1e42fea31f681968c0dc56eb12d94a81b04cf
Nicolas Suzor <nic@suzor.com>
2020-10-31 12:16:01 +1000

    preonic

--- update.sh
+++ update.sh
@@ -6,7 +6,17 @@ map {[$_, -M $_]}
 print $sorted[0]
 ' /mnt/c/Users/nicsu/Downloads/nic*plan*.json`
 echo found new keymap file: $newest_file
-cp $newest_file /mnt/e/src/qmk-nicsuzor/planck/keymap.json
+cp $newest_file keyboards/planck/keymaps/nicsuzor/keymap.json
+
+#get latest keymap
+newest_file=`perl -le '@sorted = map {$_->[0]} 
+sort {$a->[1] <=> $b->[1]} 
+map {[$_, -M $_]} 
+@ARGV;
+print $sorted[0]
+' /mnt/c/Users/nicsu/Downloads/nic*412*.json`
+echo found new keymap file: $newest_file
+cp $newest_file keyboards/boardsource/4x12/keymaps/nic/keymap.json
 
 #get latest keymap
 newest_file=`perl -le '@sorted = map {$_->[0]} 
@@ -17,7 +27,7 @@ print $sorted[0]
 ' /mnt/c/Users/nicsu/Downloads/nic_split_iris*.json`
 
 echo found new keymap file: $newest_file
-cp $newest_file /mnt/e/src/qmk-nicsuzor/iris1/keymap.json
+cp $newest_file keyboards/keebio/iris/keymaps/nicsuzor/keymap.json
 
 
 #get latest keymap
@@ -29,11 +39,11 @@ print $sorted[0]
 ' /mnt/c/Users/nicsu/Downloads/nic_split_lily*.json`
 
 echo found new keymap file: $newest_file
-cp $newest_file /mnt/e/src/qmk-nicsuzor/lily/keymap.json
+cp $newest_file keyboards/lily58/keymaps/nicsuzor/keymap.json
 
-rsync -avh /mnt/e/src/qmk-nicsuzor/iris1/ keyboards/keebio/iris/keymaps/nicsuzor/
-rsync -avh /mnt/e/src/qmk-nicsuzor/lily/ keyboards/lily58/keymaps/nicsuzor/
-rsync -avh /mnt/e/src/qmk-nicsuzor/planck/ keyboards/planck/keymaps/nicsuzor/
+#rsync -avh /mnt/e/src/qmk-nicsuzor/iris1/ keyboards/keebio/iris/keymaps/nicsuzor/
+#rsync -avh /mnt/e/src/qmk-nicsuzor/lily/ keyboards/lily58/keymaps/nicsuzor/
+##rsync -avh /mnt/e/src/qmk-nicsuzor/planck/ keyboards/planck/keymaps/nicsuzor/
 echo adding to git.
 
 git add --all keyboards/keebio/iris/keymaps/nicsuzor
