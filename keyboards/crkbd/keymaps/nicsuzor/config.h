b608a5d931846c87f625d188bc074ff0bfac3483
Nicolas Suzor <nic@suzor.com>
2020-12-20 15:13:28 +1000

    nic keymaps

---
+++ keyboards/crkbd/keymaps/nicsuzor/config.h
@@ -0,0 +1,31 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+
+//#undef USE_I2C // I2C doesn't work for split common
+
+//#    define RGB_MATRIX_SPLIT { 27, 27 }
+#    define DRIVER_LED_TOTAL 54
+#define MASTER_LEFT
+#define WS2812_DRIVER = pwm
+
+// #undef SSD1306OLED
---
+++ keyboards/crkbd/keymaps/nicsuzor/rules.mk
@@ -0,0 +1,25 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+
+CONVERT_TO_PROTON_C = yes
+
+BOOTMAGIC_ENABLE        = no
+MOUSEKEY_ENABLE         = no
+CONSOLE_ENABLE          = yes
+DEBUG_ENABLE			= yes
+COMMAND_ENABLE          = no
+AUTO_SHIFT_ENABLE = yes
+
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+
+OLED_DRIVER_ENABLE= no     # OLED display
+
+RGBLIGHT_ENABLE         = no
+RGB_MATRIX_ENABLE       = no #WS2812
+RGB_MATRIX_FRAMEBUFFER_EFFECTS = yes
+
+#SPLIT_TRANSPORT = mirror    # for when Split Mirroring drops, it will maintain mirroring functionality
+
+#RGB_MATRIX_ENABLE		= no
