053523775a7d0a4ca52f10c5faededdaf3fa7795
Nic Suzor <nic@suzor.com>
2021-10-18 18:41:23 +1000

    preonic keymap edit

--- keyboards/preonic/keymaps/nicsuzor/rules.mk
+++ keyboards/preonic/keymaps/nicsuzor/rules.mk
@@ -17,11 +17,11 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = yes           # Audio output
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 WS2812_DRIVER = bitbang
 API_SYSEX_ENABLE = no
 
-RGB_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 RGB_MATRIX_FRAMEBUFFER_EFFECTS = yes
 
