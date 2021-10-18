f33d6afd6197f442cf02513c8982dc095095153c
Nic Suzor <nic@suzor.com>
2021-10-18 17:59:54 +1000

    preonic keymap edit

--- users/nicsuzor/wrappers.h
+++ users/nicsuzor/wrappers.h
@@ -24,6 +24,8 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
 #define ________________NUMBER_LEFT________________         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5
 #define ________________NUMBER_RIGHT_______________         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC
 
+#define _______________FUNCTION_GRID_______________         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11
+
 #define _________________FUNC_1____________________         KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5
 #define _________________FUNC_2____________________         KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10
 #define _________________FUNC_3____________________         KC_TRNS, KC_F11, KC_F12,   KC_F13, KC_F14, KC_F15
