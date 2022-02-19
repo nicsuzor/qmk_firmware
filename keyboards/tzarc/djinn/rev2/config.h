92e5df040415f18444d7e9fda7febce64286af96
Nic Suzor <nic@suzor.com>
2022-02-19 18:32:25 +1000

    djinn

---
+++ keyboards/tzarc/djinn/rev2/config.h
@@ -0,0 +1,43 @@
+/* Copyright 2021 Nick Brassel (@tzarc)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// Split configuration
+#define SPLIT_HAND_PIN B9
+
+#define SERIAL_USART_DRIVER SD3
+#    define SERIAL_USART_PIN_SWAP
+#    define SERIAL_USART_TX_PIN B10
+#    define SERIAL_USART_TX_PAL_MODE 7
+#    define SERIAL_USART_RX_PIN B11
+#    define SERIAL_USART_RX_PAL_MODE 7
+
+#    define SERIAL_USART_SPEED 38400
+
+#    define SERIAL_USART_FULL_DUPLEX
+
+// Fault indicators
+#define BOARD_POWER_FAULT_PIN C9
+#define RGB_POWER_FAULT_PIN C4
+
+#ifdef AUDIO_ENABLE
+
+#define AUDIO_PIN_ALT A5
+#define AUDIO_PIN_ALT_AS_NEGATIVE
+#define STARTUP_SONG SONG(STARTUP_SOUND)
+
+#endif
---
+++ keyboards/tzarc/djinn/rules.mk
@@ -0,0 +1,32 @@
+MCU = STM32G474
+
+BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+DEBUG_ENABLE = yes
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no            # USB Nkey Rollover
+UNICODE_ENABLE = no         # Unicode
+AUDIO_ENABLE = yes           # Audio output
+
+AUDIO_DRIVER = pwm_software
+AUDIO_PIN = A4
+AUDIO_PIN_ALT = A5
+
+CFLAGS += -g$(DEBUG)
+SPLIT_KEYBOARD = yes
+OPT_DEFS += -O0
+
+ENCODER_ENABLE = yes
+
+USBPD_ENABLE = yes
+EEPROM_DRIVER = vendor
+# EEPROM_DRIVER = spi
+
+USE_FPU = yes
+LTO_ENABLE = yes
+OPT = 2
+
+DEFAULT_FOLDER = tzarc/djinn/rev2
