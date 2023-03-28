31ee0e2c3c70b2e602925fefed6759a566f554c0
Nicolas <nic@suzor.com>
2023-03-28 04:53:22 +0000

    everything compiles and serial works!

--- users/nic/wrappers.h
+++ users/nic/wrappers.h
@@ -33,11 +33,11 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
 
 #define ___________________BLANK___________________         _______, _______, _______, _______, _______, _______
 
-#define _______________GRID_BOTTOM_ROW_____________         KC_LCTL, KC_LGUI, DEBUG, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT
+#define _______________GRID_BOTTOM_ROW_____________         KC_LCTL, KC_LGUI, QK_DEBUG_TOGGLE, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT
 #define _____________DOUBLE_BOTTOM_ROW_____________         KC_LCTL, KC_LGUI, KC_NO,  KC_LALT,LOWER,     KC_SPC,     RAISE, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT
 
 #define ________________NAV_LEFT_1_________________         EPRM, KC_HOME, KC_UP, KC_END, KC_PGUP,  KC_BSPC //????
-#define ________________NAV_LEFT_2_________________         RESET, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  KC_ENT //????
+#define ________________NAV_LEFT_2_________________         QK_BOOT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  KC_ENT //????
 
 #define _________________NUMPAD_1__________________         ________________NUMBER_RIGHT_______________
 #define _________________NUMPAD_2__________________         KC_PEQL, KC_4, KC_5, KC_6, KC_PAST, KC_ENT
@@ -66,13 +66,13 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
 #define _________________RAISE_R4__________________         _________________NUMPAD_3__________________
 
 #define _________________ADJUST_L1_________________         ___________________BLANK___________________
-#define _________________ADJUST_L2_________________         RGB_MOD, RGB_TOG, RGBRST, VRSN, DEBUG, EPRM
-#define _________________ADJUST_L3_________________         MU_TOG , CK_TOGG, AU_ON,   AU_OFF,  CG_NORM, RESET
+#define _________________ADJUST_L2_________________         RGB_MOD, RGB_TOG, RGBRST, VRSN, QK_DEBUG_TOGGLE, EPRM
+#define _________________ADJUST_L3_________________         MU_TOGG , CK_TOGG, AU_ON,   AU_OFF,  CG_NORM, QK_BOOT
 #define _________________ADJUST_L4_________________         DBG_KEY,DBG_MX,RGB_SAD, RGB_VAD, RGB_TOG, RGB_IDL
 
 #define _________________ADJUST_R1_________________         ___________________BLANK___________________
-#define _________________ADJUST_R2_________________         KC_NO, KC_NO,  EPRM, KC_MAKE,  DEBUG, RESET
-#define _________________ADJUST_R3_________________         RGB_MOD, KC_MAKE,  DEBUG, RESET,EPRM,RGB_TOG
+#define _________________ADJUST_R2_________________         KC_NO, KC_NO,  EPRM, KC_MAKE,  QK_DEBUG_TOGGLE, QK_BOOT
+#define _________________ADJUST_R3_________________         RGB_MOD, KC_MAKE,  QK_DEBUG_TOGGLE, QK_BOOT,EPRM,RGB_TOG
 #define _________________ADJUST_R4_________________         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0)
 
 // clang-format on
