72613369af4b3ee4bf5142b52586f40cde41ddf0
Nicolas Suzor <nic@suzor.com>
2024-07-03 05:18:18 +0000

    no oled

--- keyboards/crkbd/ns/rules.mk
+++ keyboards/crkbd/ns/rules.mk
@@ -1,8 +1,7 @@
 
 DEBUG_ENABLE = yes
-FIRMWARE_FORMAT = bin
-BOOTLOADER = stm32-dfu
-BOOTLOADER_TYPE = stm32_dfu
-SPLIT_KEYBOARD = yes
 CONSOLE_ENABLE   = yes  # Console for debug(+400)
 COMMAND_ENABLE   = yes # Commands for debug and configuration
+
+OLED_ENABLE = no
+MCU = STM32F303
